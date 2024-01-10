cba661ad9ff9e8b2190c9eaeba30904e756d88da
unknown <crtaylorlovesstl@gmail.com>
2024-01-09 19:39:39 -0600

    Finished exercises

--- booleans-and-conditionals/exercises/part-1.js
+++ booleans-and-conditionals/exercises/part-1.js
@@ -1,4 +1,18 @@
 // Declare and initialize the variables for exercise 1 here:
+let engineIndicatorLight = 'red blinking';
+let spaceSuitsOn = true;
+let shuttleCabinReady = true;
+
+
+let computerStatusCode = 200;
+let shuttleSpeed = 15000;
+let crewStatus = false;
+
+if (spaceSuitsOn && shuttleCabinReady) {
+   crewStatus = true;
+}
+
+
 
 // BEFORE running the code, predict what will be printed to the console by the following statements:
 
