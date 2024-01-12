658aedaa149f898416af9ebf0d08e15260a907ca
unknown <crtaylorlovesstl@gmail.com>
2024-01-11 20:15:21 -0600

    Finished

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -1,6 +1,50 @@
 // Initialize Variables below
+let date = 'Monday 2019-03-18';
+let time = '10:05:34 AM';
+let astronautCount = 7;
+let astronautStatus = 'Ready';
+let averageAstronautMassKg = 80.7
+let crewMassKg = astronautCount * averageAstronautMassKg;
+let fuelMassKg = 760000;
+let shuttleMassKg = 74842.31;
+let totalMassKg = crewMassKg + fuelMassKg + shuttleMassKg;
+let maximumMassLimit = 850000;
+let fuelTempCelsius = -225;
+let minimumFuelTemp = -300;
+let maximumFuelTemp = -150;
+let fuelLevel = '100%';
+let weatherStatus = 'Clear';
+let preparedForLiftOff = true;
 
 // add logic below to verify total number of astronauts for shuttle launch does not exceed 7
+if (astronautCount <= 7) {
+    if (astronautStatus === 'Ready') 
+        if (totalMassKg < maximumMassLimit)
+            if (fuelTempCelsius > -300 && fuelTempCelsius < -150)
+                if (fuelLevel === '100%')
+                    if (weatherStatus === 'Clear')
+                    preparedForLiftOff == true;
+} 
+
+if (preparedForLiftOff) {
+    console.log(date);
+    console.log(time);
+    console.log(astronautCount);
+    console.log(crewMassKg);
+    console.log(fuelMassKg);
+    console.log(shuttleMassKg);
+    console.log(totalMassKg);
+    console.log(fuelTempCelsius);
+    console.log(weatherStatus);
+    console.log('Have a safe flight');
+} else {
+    console.log('Launch scrubbed.');
+
+
+
+}
+
+
 
 // add logic below to verify all astronauts are ready
 
