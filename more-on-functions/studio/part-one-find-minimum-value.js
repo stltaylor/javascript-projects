08c8dd7aae87363d30ad2431de4bf4b25dde3ddf
unknown <crtaylorlovesstl@gmail.com>
2024-01-29 21:24:13 -0600

    sort

--- more-on-functions/studio/part-one-find-minimum-value.js
+++ more-on-functions/studio/part-one-find-minimum-value.js
@@ -7,4 +7,49 @@ let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
 //Using one of the test arrays as the argument, call your function inside the console.log statement below.
 
-console.log(/* your code here */);
+// console.log(nums1.sort());
+
+
+// function minArr(arr) {
+//     for (let i = 0; i < arr.length - 1; i++) {
+//         for (let j = 0; j < arr.length - 1 - i; j++) {
+//           if (arr[j] > arr[j + 1]) {
+           
+//             newArr = arr[j];
+//             arr[j] = arr[j + 1];
+//             newArr = arr[j+1]
+//     return newArr
+// }
+
+
+
+
+function minArr(arr) {
+    let newArr = arr[0]
+    
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < newArr) {
+            newArr = arr[i];
+        }
+        }
+    return newArr
+}
+
+function sortArr(arr) {
+    let arry1 = []
+    let arry2 = []
+    while (arr.length > 0) {
+        arry2 = minArr(arr)
+        arry1.push(arry2)
+        arr.splice(arr.indexOf(arry2), 1)
+
+    }
+    return arry1
+
+}
+
+
+
+
+console.log(sortArr(nums1))
+console.log(nums1)
--- more-on-functions/studio/part-three-number-sorting-easy-way.js
+++ more-on-functions/studio/part-three-number-sorting-easy-way.js
@@ -4,5 +4,32 @@ let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
 //Sort each array in ascending order.
+function maxArr(arr) {
+    let newArr = arr[0]
+    
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > newArr) {
+            newArr = arr[i];
+        }
+        }
+    return newArr
+}
 
-//Sort each array in decending order.
+function sortArr(arr) {
+    let arry1 = []
+    let arry2 = []
+    while (arr.length > 0) {
+        arry2 = maxArr(arr)
+        arry1.push(arry2)
+        arr.splice(arr.indexOf(arry2), 1)
+
+    }
+    return arry1
+
+}
+
+
+
+
+console.log(sortArr(nums1))
+console.log(nums1)
