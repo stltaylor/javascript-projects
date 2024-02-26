5316eb86b2c1678bf976ce17f3952e7e2578b935
unknown <crtaylorlovesstl@gmail.com>
2024-02-26 17:21:33 -0600

    domdomdomdom

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -2,9 +2,20 @@ function init () {
     const missionAbort = document.getElementById("abortMission");
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
-
-    // Put your code for the exercises here.
+    const main = document.getElementById("main")
     
+    button.addEventListener("click", (event) => {
+      paragraph.innerHTML = "Houston! We have liftoff!";
+    });
+    missionAbort.addEventListener("mouseover", function (event) {
+       main.style.backgroundColor = "red";
+    });
+    missionAbort.addEventListener("mouseout", function() {
+        main.style.backgroundColor = ''
+    });
+    missionAbort.addEventListener("click", function(){
+        window.confirm("Are you sure you want to abort the mission?")
+    })
 }
 
 window.addEventListener("load", init);
